f4c99d1416572b329b36178b8eb0854510675965
Dohun <hun4215@hanmail.net>
2023-05-16 15:51:51 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express')
 const bodyparser = require('body-parser')
 const app = express();
@@ -6,37 +7,29 @@ const port = process.env.PORT || 5000;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 
-app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday': '961222',
-            'gender': '남자',
-            'job': '대학생'
-          
-          },
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '현도훈',
-            'birthday': '960627',
-            'gender': '남자',
-            'job': '대학생'
-          
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '윤지은',
-            'birthday': '960829',
-            'gender': '여자',
-            'job': '대학생'
-          
-          }]
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+
+});
+connection.connect();
+
+app.get('/api/customers', (req, res) => {
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+
+        }
 
     );
 });
+
 app.listen(port, ()=> console.log(`listening on port ${port}`));
