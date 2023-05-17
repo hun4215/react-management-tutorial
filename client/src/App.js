d2084306558e87439a3f74567b969c8b0278bbb1
Dohun <hun4215@hanmail.net>
2023-05-17 13:54:13 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
+import Customer from './components/Customer'
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
-import Customer from './components/Customer'
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
@@ -27,9 +28,22 @@ const styles = theme => ({
 
 class App extends Component {
 
-  state = {
-    customers: "",
-    completed: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
   }
 
   componentDidMount() {
@@ -54,35 +68,37 @@ class App extends Component {
   render() {
     const {classes} = this.props;
      return (
-      <Paper className={classes.root}>
-        <Table classNmae={classes.table}>
-          <TableHead>
-            <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-                {this.state.customers ? this.state.customers.map(c => {
-                  return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}> </Customer> )
-                }) : 
-                <TableRow>
-                  <TableCell colSpan="6" align="center">
-                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+      <div>
+        <Paper className={classes.root}>
+          <Table classNmae={classes.table}>
+            <TableHead>
+              <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+                  {this.state.customers ? this.state.customers.map(c => {
+                    return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}> </Customer> )
+                  }) : 
+                  <TableRow>
+                    <TableCell colSpan="6" align="center">
+                      <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
 
 
-                  </TableCell>
+                    </TableCell>
 
-                </TableRow>
-                }
-          </TableBody>
-        </Table>
-        
-      </Paper>
+                  </TableRow>
+                  }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh}/>
+      </div>
      );
   }
 }
